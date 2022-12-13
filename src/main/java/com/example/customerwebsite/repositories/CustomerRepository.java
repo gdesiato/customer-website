b5075775edab08e46f7691df0f0ba3408e76255e
Giuseppe Desiato <giodesiato@gmail.com>
2022-12-12 21:46:41 -0500

    assign car

--- src/main/java/com/example/customerwebsite/repositories/CustomerRepository.java
+++ src/main/java/com/example/customerwebsite/repositories/CustomerRepository.java
@@ -3,6 +3,6 @@ package com.example.customerwebsite.repositories;
 import com.example.customerwebsite.model.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-// The rpository allow us to have all the functionalities, such as finding, saving, updating, etc.
+// The repository allow us to have all the functionalities, such as finding, saving, updating, etc.
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
 }
